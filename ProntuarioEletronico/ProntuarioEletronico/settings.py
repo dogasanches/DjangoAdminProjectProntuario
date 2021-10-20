812e2cbdd409a628f0eb990790ad832211d0cbe8
dogasanches <2015416@aluno.univesp.br>
2021-10-20 15:49:37 -0300

    Configuração dos parâmetros MySQL da aplicação

--- ProntuarioEletronico/ProntuarioEletronico/settings.py
+++ ProntuarioEletronico/ProntuarioEletronico/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'ProntuarioEletronico.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ProntuarioEletronico',
+        'USER': 'root',
+        'PASSWORD': 'Admin@1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
