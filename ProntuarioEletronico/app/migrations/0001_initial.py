0347f09e79db9be6229ac849eb751d4358bfa4ba
dogasanches <2015416@aluno.univesp.br>
2021-10-22 11:07:42 -0300

    Registro dos Models no django admin Criado super user no Django

--- ProntuarioEletronico/app/migrations/0001_initial.py
+++ ProntuarioEletronico/app/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Consultas',
+            name='Consultas Pront',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('sintomas', models.CharField(max_length=1000)),
