98f118834238ff3123c6435c72af6b63ac45c576
dogasanches <2015416@aluno.univesp.br>
2021-10-25 09:14:22 -0300

    Config colunas Djangoadmin

--- ProntuarioEletronico/app/admin.py
+++ ProntuarioEletronico/app/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from .models import *
+from .models import Consulta, Paciente
+
+#from .models import *
 
 # Register your models here.
 
-admin.site.register(Paciente)
-admin.site.register(Consulta)
+#admin.site.register(Paciente)
+#admin.site.register(Consulta)
 
+@admin.register(Consulta)
+class ConsultaAdmin (admin.ModelAdmin):
+    list_display = ("Paciente",'sintomas','tipo','especialidade','dataconsulta','arquivo1','arquivo2','arquivo3',)
+    search_fields = ("sintomas",)
+    list_filter = ('dataconsulta',)
+    date_hierarch = ('dataconsulta')
+
+@admin.register(Paciente)
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ("nome",'data_nascimento',)
+    search_fields = ("nome",)
+    list_filter = ('nome',)
+    date_hierarch = ('nome')
